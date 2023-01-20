7e7e5ab8927c24cd2531815502a0d545593207f1
exp-ext <ext77@yandex.ru>
2023-01-20 18:29:25 +0300

    update

--- todo/about/urls.py
+++ todo/about/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from .views import AboutProjectView
 
 urlpatterns = [
-    path('contacts/', AboutProjectView.as_view(), name='project'),
+    path('contacts/', AboutProjectView.as_view(), name='support'),
 ]

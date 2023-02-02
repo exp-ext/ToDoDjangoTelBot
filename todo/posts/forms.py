9472e808ecf81ec7dd451d00b82504819830db1a
exp-ext <ext77@yandex.ru>
2023-02-02 10:22:57 +0300

    update

--- todo/posts/forms.py
+++ todo/posts/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 from django.forms import Textarea
+from django.shortcuts import get_object_or_404
+from users.models import Group
 
 from .models import Comment, Post
 
@@ -21,6 +23,12 @@ class PostForm(forms.ModelForm):
         self.fields['group'].help_text = (
             'Группа, к которой будет относиться пост'
         )
+
+    def clean_group(self):
+        group = self.cleaned_data['group']
+        if group:
+            return get_object_or_404(Group, pk=group.group_id)
+        return group
 
 
 class CommentForm(forms.ModelForm):

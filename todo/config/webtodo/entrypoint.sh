5650dfbe9726e2e2a774a3091963db3ddd5e1935
exp-ext <ext77@yandex.ru>
2023-03-26 21:43:43 +0300

    update

--- todo/config/webtodo/entrypoint.sh
+++ todo/config/webtodo/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-until cd /app/todo/
+until cd /app/
 do
     echo "Waiting for server volume..."
 done
@@ -8,12 +8,6 @@ done
 until python3 manage.py migrate
 do
     echo "Waiting for db to be ready... No migrate..."
-    sleep 2
-done
-
-until python3 manage.py set_ip_by_host
-do
-    echo "Waiting seting IP..."
     sleep 2
 done
 

e44291375d68261d52db49dabb3960b990baa510
exp-ext <ext77@yandex.ru>
2023-04-09 09:44:37 +0300

    add test

---
+++ todo/posts/tests/test_models.py
@@ -0,0 +1,31 @@
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+from posts.models import Post
+
+User = get_user_model()
+
+
+class PostModelTest(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.user = User.objects.create_user(
+            username='auth',
+            password='1234GLKLl5',
+        )
+        cls.post = Post.objects.create(
+            author=cls.user,
+            title='title777',
+            text='Тестовый пост 777. ' * 3
+        )
+
+    def test_posts_have_correct_object_names(self):
+        """Проверяем, что у модели корректно работает __str__."""
+        post = self.post
+        str_text = post.title[:20]
+        self.assertEqual(
+            str_text,
+            str(post),
+            f'У модели Post результат __str__ = "{str_text}" '
+            f'не соответствует ожидаемому "{str(post)}"'
+        )

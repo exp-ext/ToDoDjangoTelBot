92f280af1784f91e040ee4c6b668ffe8b3a0da9e
exp-ext <ext77@yandex.ru>
2023-10-13 08:18:01 +0300

    many change

--- todo/todo/settings.py
+++ todo/todo/settings.py
@@ -14,7 +14,6 @@ import os
 import socket
 from pathlib import Path
 
-import boto3
 import redis
 from core.keygen import get_key
 from dotenv import load_dotenv
@@ -215,54 +214,20 @@ USE_L10N = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
-AWS_S3_ENDPOINT_URL = os.getenv('AWS_S3_ENDPOINT_URL')
-AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
-AWS_S3_USE_SSL = int(os.getenv('AWS_S3_USE_SSL', default=0))
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = Path(BASE_DIR).joinpath('media').resolve()
 
-STATIC_BUCKET_NAME = 'static'
-MEDIA_BUCKET_NAME = 'media'
-DATABASE_BUCKET_NAME = 'database'
+DBBACKUP_STORAGE = 'django.core.files.storage.FileSystemStorage'
 
-USE_S3 = int(os.getenv('USE_S3', default=0))
-if USE_S3:
-    STATIC_URL = f'{AWS_S3_ENDPOINT_URL}/{AWS_STORAGE_BUCKET_NAME}/{STATIC_BUCKET_NAME}/'
-    STATICFILES_STORAGE = 'todo.boto.StaticStorage'
-
-    MEDIA_URL = f'{AWS_S3_ENDPOINT_URL}/{AWS_STORAGE_BUCKET_NAME}/{MEDIA_BUCKET_NAME}/'
-    DEFAULT_FILE_STORAGE = 'todo.boto.MediaStorage'
-
-    DBBACKUP_STORAGE = 'todo.boto.DataBaseStorage'
-    DBBACKUP_STORAGE_OPTIONS = {
-        'access_key': AWS_ACCESS_KEY_ID,
-        'secret_key': AWS_SECRET_ACCESS_KEY,
-        'bucket_name': AWS_STORAGE_BUCKET_NAME,
-        'default_acl': 'private',
-    }
-else:
-    STATIC_URL = '/static/'
-
-    MEDIA_URL = '/media/'
-    MEDIA_ROOT = Path(BASE_DIR).joinpath('media').resolve()
-
-    DBBACKUP_STORAGE = 'django.core.files.storage.FileSystemStorage'
-    DBBACKUP_STORAGE_OPTIONS = {
-        'location': Path(BASE_DIR).joinpath('backup').resolve()
-    }
+DBBACKUP_STORAGE_OPTIONS = {
+    'location': Path(BASE_DIR).joinpath('backup').resolve()
+}
 
 if DEBUG:
     STATICFILES_DIRS = (BASE_DIR / 'static',)
 else:
     STATIC_ROOT = Path(BASE_DIR).joinpath('static').resolve()
-
-S3_CLIENT = boto3.client(
-    's3',
-    use_ssl=AWS_S3_USE_SSL,
-    aws_access_key_id=AWS_ACCESS_KEY_ID,
-    aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
-    endpoint_url=AWS_S3_ENDPOINT_URL
-)
 
 # Django-ckeditor
 CKEDITOR_5_FILE_STORAGE = 'posts.storage.CkeditorCustomStorage'
@@ -357,13 +322,6 @@ CKEDITOR_5_CONFIGS = {
         }
     }
 }
-
-if USE_S3:
-    CKEDITOR_CONFIGS = {
-        "default": {
-            "removePlugins": "stylesheetparser",
-        }
-    }
 
 
 # Setting for working with Jupiter

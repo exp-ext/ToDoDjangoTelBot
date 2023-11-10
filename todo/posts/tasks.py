b46d8555c53ea1c51fbd873294d4d559b2fa5e51
exp-ext <ext77@yandex.ru>
2023-11-10 18:36:28 +0300

    contents

--- todo/posts/tasks.py
+++ todo/posts/tasks.py
@@ -14,26 +14,23 @@ def create_contents(post_model: Dict[str, Any]) -> str:
     soup = BeautifulSoup(text, features="html.parser")
     current_node_db = None
     instance.contents.all().delete()
-    contents = []
 
     base_node_db = PostContents.objects.create(post=instance, anchor=instance.title, is_root=True)
 
     for tag in soup.find_all(['h2', 'h4']):
+        tag_text = tag.text.strip()
+        search_tag = str(tag).replace('\xa0', '&nbsp;')
+        text = text.replace(search_tag, f'<section id="{tag_text}">{str(tag)}</section>')
+
         if tag.name == 'h2':
-            text = text.replace(str(tag), f'<section id="{tag.text}">{str(tag)}</section>')
-            current_node_db = PostContents.objects.create(post=instance, anchor=tag.text, parent=base_node_db)
+            current_node_db = PostContents.objects.create(post=instance, anchor=tag_text, parent=base_node_db)
         elif tag.name == 'h4':
-            text = text.replace(str(tag), f'<section id="{tag.text}">{str(tag)}</section>')
-            contents.append(
-                PostContents(
-                    post=instance,
-                    anchor=tag.text,
-                    parent=current_node_db,
-                )
+            PostContents.objects.create(
+                post=instance,
+                anchor=tag_text,
+                parent=current_node_db if current_node_db else base_node_db,
             )
 
-    if contents:
-        PostContents.objects.bulk_create(contents)
     instance.text = text
     instance.save()
     return 'Создано оглавление для поста '

41890cb672679de8e2111082c0db532857c9d5ac
exp-ext <ext77@yandex.ru>
2023-12-14 11:25:27 +0300

    update post create

--- todo/ai/routing.py
+++ todo/ai/routing.py
@@ -1,8 +1,7 @@
 from ai.consumers import ChatConsumer
-from django.conf import settings
 from django.urls import re_path
 
-urlpatterns = r'(?P<room_name>[0-9a-f-]+)/$' if settings.DEBUG else r'ws/(?P<room_name>[0-9a-f-]+)/$'
+urlpatterns = r'ws/(?P<room_name>[0-9a-f-]+)/$'
 
 websocket_urlpatterns = [
     re_path(urlpatterns, ChatConsumer.as_asgi()),

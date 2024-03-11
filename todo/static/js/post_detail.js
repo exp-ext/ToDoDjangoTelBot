694c6f2f12fb72a7d3b061954d17a6b6b82f7fdb
exp-ext <ext77@yandex.ru>
2024-03-11 10:35:43 +0300

    update upload cdn

--- todo/static/js/post_detail.js
+++ todo/static/js/post_detail.js
@@ -55,7 +55,7 @@ document.addEventListener("DOMContentLoaded", function() {
       requestAnimationFrame(updateAdBlockContent);
     }
   }
-  setTimeout(checkAdBlock, 1500);
+  setTimeout(checkAdBlock, 2500);
 });
 $(document).ready(function() {
   $('.counter').each(function () {
@@ -104,9 +104,16 @@ function toggleList(event, toggleIcon) {
   if (ul) {
     ul.classList.toggle('hidden');
     ul.classList.toggle('visible');
-    const icon = toggleIcon.querySelector('i');
-    icon.classList.toggle('bi-plus-circle');
-    icon.classList.toggle('bi-circle');
+    const svg = toggleIcon.querySelector('svg');
+    if (svg.classList.contains('bi-plus-circle')) {
+      svg.setAttribute('viewBox', "0 0 16 16");
+      svg.innerHTML = '<path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>';
+      svg.classList.replace('bi-plus-circle', 'bi-circle');
+    } else {
+      svg.setAttribute('viewBox', "0 0 16 16");
+      svg.innerHTML = '<path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/><path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>';
+      svg.classList.replace('bi-circle', 'bi-plus-circle');
+    }
   }
 }
 document.addEventListener('DOMContentLoaded', (event) => {

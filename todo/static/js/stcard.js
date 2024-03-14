e8d69280fc8be7b8ef17a9a128996c285791e47e
exp-ext <ext77@yandex.ru>
2024-03-14 11:11:56 +0300

    update stcard

--- todo/static/js/stcard.js
+++ todo/static/js/stcard.js
@@ -2,3 +2,39 @@ document.querySelectorAll(".stcard").forEach(stcard => {
     const rotate = Math.random() * 2 * (Math.round(Math.random()) ? 1 : -1);
     stcard.style.transform = `rotate(${rotate}deg)`;
 });
+function truncateHtml(selector, maxLength) {
+  var elements = document.querySelectorAll(selector);
+  elements.forEach(function(element) {
+    var walk = document.createTreeWalker(element, NodeFilter.SHOW_TEXT, null, false);
+    var totalLength = 0;
+    var charCount;
+    var nodesToRemove = [];
+    while (walk.nextNode()) {
+      var currentNode = walk.currentNode;
+      totalLength += currentNode.length;
+      if (totalLength > maxLength) {
+        charCount = maxLength - (totalLength - currentNode.length);
+        currentNode.nodeValue = currentNode.nodeValue.substr(0, charCount) + '...';
+        nodesToRemove.push(currentNode);
+        break;
+      }
+    }
+    for (var i = 0; i < nodesToRemove.length; i++) {
+      var nodeToRemove = nodesToRemove[i];
+      while (nodeToRemove.nextSibling) {
+        nodeToRemove.parentNode.removeChild(nodeToRemove.nextSibling);
+      }
+    }
+    var walker = document.createTreeWalker(element, NodeFilter.SHOW_ELEMENT, null, false);
+    var node;
+    while (walker.nextNode()) {
+      node = walker.currentNode;
+      if (node.hasChildNodes() && !node.firstChild.nodeValue.trim()) {
+        node.parentNode.removeChild(node);
+      }
+    }
+  });
+}
+document.addEventListener('DOMContentLoaded', function() {
+  truncateHtml('.stcard p', 65);
+});
